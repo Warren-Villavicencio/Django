35a3512a4013cb1d7ec34806657bfcc19552f5c2
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-15 22:49:57 -0500

    Auto-commit: Actualización de código

--- 01-ciclo/04-ejercicio.py
+++ 01-ciclo/04-ejercicio.py
@@ -4,6 +4,6 @@
 
 name = str(input("¿Cuál es tu nombre? "))
 edad = int(input("¿Qué edad tienes? "))
-altura = float(input("¿Cuál es tu estatura "))
+altura = float(input("¿Cuál es tu estatura? "))
 
 print (f"tu nombre es : {name} tienes {edad} y mides {altura}")
