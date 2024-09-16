a46fb1ff65104e94847861bb41e7d5f380fd4ff2
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-15 22:40:35 -0500

    Auto-commit: Actualización de código

--- 01-ciclo/02-Ejercicio.py
+++ 01-ciclo/02-Ejercicio.py
@@ -1,3 +1,3 @@
 #Escribir un programa que guarde en variable la cadena ¡Estoy aprendiendo Python! Y que luego muestre en pantalla el valor de esa variable.
 guardar_cadena = "¡Estoy aprendiendo Python!"
-print(f"")
+print(f"{}")
