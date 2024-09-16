78be18efbd77dcd87d695cc4394bb900ef268879
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-15 22:46:01 -0500

    Auto-commit: Actualización de código

--- 01-ciclo/04-ejercicio.py
+++ 01-ciclo/04-ejercicio.py
@@ -3,5 +3,5 @@
 # un valor entero y un string
 
 name = str(input("¿Cuál es tu nombre? "))
-name = str(input("¿Cuál es tu nombre? "))
+edad = str(input("¿Cuál es tu nombre? "))
 name = str(input("¿Cuál es tu nombre? "))
