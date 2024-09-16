327fb8e60430fdcc61570237c8a3dd035ebef8a9
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-15 22:43:29 -0500

    Auto-commit: Actualización de código

--- 01-ciclo/03-ejercicio.py
+++ 01-ciclo/03-ejercicio.py
@@ -1,4 +1,4 @@
 # Escriba un programa que le pregunte al usuario su nombre para luego saludarlo por consola por su nombre
 
 name = imput("¿Dime cuál es tu nombre? ") 
-print(f"")
+print(f"Bienvenido")
