d8c4d80c3eae06063c1c79bdcdfc745ab61a883f
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-15 23:11:26 -0500

    Auto-commit: Actualización de código

--- 02-ciclo/operadores-logicos.py
+++ 02-ciclo/operadores-logicos.py
@@ -18,6 +18,6 @@ print(resultado)
 valor_uno = float(input("Ingresa el primer valor ")) 
 valor_dos = float(input("Ingrese el segundo valor ")) 
 resultado = valor_dos and valor_dos
-print(comparacion)
+print(resultado)
 
 
