2a2599cbe3f625192d18ce1d449da2d4a50aa350
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-15 22:59:48 -0500

    Auto-commit: Actualización de código

--- 01-ciclo - copia/Variables-numericas-texto.py
+++
@@ -1,9 +0,0 @@
-# Ejemplo de uso de variables numéricas y de texto
-# Este código calcula el perímetro de un círculo
-
-radio = 5.0  # Variable de tipo float para el radio
-pi = 3.14159  # Constante matemática pi
-diametro = 2 * radio  # Calculamos el diámetro
-perimetro = pi * diametro  # Calculamos el perímetro
-
-print("El perímetro del círculo es:", perimetro)
